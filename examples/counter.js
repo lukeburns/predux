a465a96ce94e753fdd427299e408fb5e6390d1b8
Luke Burns <lukemburns@gmail.com>
2016-06-03 23:03:19 -0400

    updated reduxer version

--- examples/counter.js
+++ examples/counter.js
@@ -3,8 +3,6 @@ var stringify = require('JSONStream').stringify(false)
 
 var counter = predux.proxy()
 
-// counter.set('increment', (state=0, num=1) => state+num)
-// counter.set('decrement', (state=0, num=1) => state-num)
 counter.increment = (state=0, num=1) => state+num
 counter.decrement = (state=0, num=1) => state-num
 
